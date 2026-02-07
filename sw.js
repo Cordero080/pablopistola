388045682f055b2d0bdeb0f1fbd7255eab0428f7
Pablo Cordero <cordero080@gmail.com>
2026-02-07 14:59:21 -0500

    Make landing page the entry point

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "pablo-pistola-v3";
+const CACHE_NAME = "pablo-pistola-v4";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
-  "./landing.html",
+  "./home.html",
   "./app.js",
   "./Css/style.css",
   "./Css/landing.css",
