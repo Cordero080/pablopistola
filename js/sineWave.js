693c26742c352e640877ecda162b3fa62adda431
Pablo Cordero <cordero080@gmail.com>
2026-02-13 05:15:07 -0500

    changes

--- js/sineWave.js
+++ js/sineWave.js
@@ -201,7 +201,7 @@ window.waveGen = new SineWaveGenerator({
   waves: [
     {
       timeModifier: 1,
-      lineWidth: 44.5,
+      lineWidth: 22.5,
       amplitude: 150,
       wavelength: 300,
       segmentLength: 20,
