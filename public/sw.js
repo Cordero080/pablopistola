7050244dfa2dfceb3a128c2223abbd2d53d97372
Pablo Cordero <cordero080@gmail.com>
2026-05-18 15:50:01 -0400

    fix: remove ASCII canvas from landing, dead code cleanup, PWA landing page   fixes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "pablo-pistola-1778556340747";
+const CACHE_NAME = "pablo-pistola-1747612800000";
 const ASSETS_TO_CACHE = [
+  "/",
   "/home.html",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
